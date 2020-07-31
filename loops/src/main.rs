b055bf3afe1216ca48f03548c49bbc03b1df92a4
Albert Abdulmanov <albert.abdulmanov@team.wrike.com>
2020-07-31 23:37:26 +0500

    learn loops

---
+++ loops/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    my_loop();
+    my_while();
+    my_for();
+}
+
+fn my_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn my_while() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn my_for() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+}
