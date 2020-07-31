21715b54abc167e4cc0f69ce9d3d31f3157ad3f2
Albert Abdulmanov <albert.abdulmanov@team.wrike.com>
2020-08-01 02:43:54 +0500

    learn structures part 3, try implementations and associative functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,15 +4,35 @@ struct Rectangle {
     height: u32,
 }
 
-fn calc_area(rec: &Rectangle) -> u32 {
-    rec.width * rec.height
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
-    let rectangle = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("rectangle is {:#?}", rectangle);
-    println!("Area of rectangle is {}", calc_area(&rectangle))
+
+    let rect2 = Rectangle {
+        width: 40,
+        height: 60,
+    };
+    println!("Can rect 1 hold rect 2? {}", rect2.can_hold(&rect1));
+
+    let sq1 = Rectangle::square(20);
+    println!("{:#?}", sq1)
 }
