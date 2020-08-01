7ffab470993129a87900a2b25f82a96671e017c3
Albert Abdulmanov <albert.abdulmanov@team.wrike.com>
2020-08-01 22:37:20 +0500

    try match Option

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -18,6 +18,38 @@ impl Message {
     }
 }
 
+#[derive(Debug)] // Можно проверить значение штата в списке
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("Quarter!! {:?}", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6("::1".to_string());
@@ -25,4 +57,12 @@ fn main() {
 
     let msg = Message::Write(String::from("Hello"));
     msg.call();
+
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
 }
